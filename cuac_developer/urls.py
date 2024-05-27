46014cdc5f240aefbca9e387bc826249fa0b3817
julio <julioamayamlg@gmail.com>
2024-05-27 12:11:25 +0200

    model changes, batch and task list created

--- cuac_developer/urls.py
+++ cuac_developer/urls.py
@@ -11,11 +11,15 @@ urlpatterns = [
     path('batch/<int:pk>/', views.BatchDetail.as_view(), name='batch-detail'),
     path('batch/<int:pk>/update/', views.BatchUpdate.as_view(), name='batch-update'),
     path('batch/<int:pk>/delete/', views.BatchDelete.as_view(), name='batch-delete'),
-
+    path('batch/asjson/', views.BatchListAsJSON.as_view(), name='batch-list-asjson'),
+    path('batch/editor/', views.BatchEditor.as_view(), name='batch-editor'),
 
     path('tasks/', views.TaskList.as_view(), name='task-list'),
     path('task', views.TaskCreate.as_view(), name='task-create'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task-detail'),
     path('task/<int:pk>/update/', views.TaskUpdate.as_view(), name='task-update'),
     path('task/<int:pk>/delete/', views.TaskDelete.as_view(), name='task-delete'),
+    path('task/asjson/', views.TaskListAsJSON.as_view(), name='task-list-asjson'),
+    path('task/editor/', views.TaskEditor.as_view(), name='task-editor'),
+
 ]
