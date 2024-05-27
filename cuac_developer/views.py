5f804851cfc7230b2f833c04d96d0060b7281ecf
julio <julioamayamlg@gmail.com>
2024-05-27 12:11:58 +0200

    update views.py

--- cuac_developer/views.py
+++ cuac_developer/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
-from braces.views import LoginRequiredMixin
+from braces.views import LoginRequiredMixin, SuperuserRequiredMixin, PermissionRequiredMixin, JSONResponseMixin
+from cuac_core.mixins import DatatableListView, DatatableEditor
+from django.urls import reverse_lazy
 from . import models, forms
 
 
@@ -25,6 +27,11 @@ class BatchDelete(LoginRequiredMixin, generic.DeleteView):
     model = models.Batch
 
 
+class BatchEditor(LoginRequiredMixin,
+                    DatatableEditor):
+    model = models.Batch
+
+
 class TaskList(LoginRequiredMixin, generic.ListView):
     model = models.Task
 
@@ -45,3 +52,70 @@ class TaskUpdate(LoginRequiredMixin, generic.UpdateView):
 
 class TaskDelete(LoginRequiredMixin, generic.DeleteView):
     model = models.Task
+
+
+class TaskEditor(LoginRequiredMixin,
+                    DatatableEditor):
+    model = models.Task
+
+
+class TaskListAsJSON(LoginRequiredMixin, DatatableListView):
+    model = models.Task
+    column_names_map = {
+    }
+
+    @staticmethod
+    def get_actions(obj):
+        return (f"<a href='{reverse_lazy('cuac_core:task-detail', kwargs={'pk': obj.pk})}' "
+                f"class='btn btn-primary'>"
+                f"<i class='bi bi-eye'></i>"
+                f"</a>"
+                f"<a href='{reverse_lazy('cuac_core:task-delete', kwargs={'pk': obj.pk})}' "
+                f"class='btn btn-danger'>"
+                f"<i class='bi bi-trash'></i>"
+                f"</a>")
+
+    def object_wrapper(self, obj):
+        row = {
+            'DT_RowId': obj.pk,
+            'title': obj.title,
+            'description': obj.description,
+            'time': obj.time,
+            'batch': obj.batch.pk,
+            'assignee': obj.assignee.pk,
+            'assignee_name': obj.assignee.first_name,
+        }
+        return row
+
+
+class BatchListAsJSON(LoginRequiredMixin, DatatableListView):
+    model = models.Batch
+    column_names_map = {
+    }
+
+    @staticmethod
+    def get_actions(obj):
+        return (f"<a href='{reverse_lazy('cuac_core:batch-detail', kwargs={'pk': obj.pk})}' "
+                f"class='btn btn-primary'>"
+                f"<i class='bi bi-eye'></i>"
+                f"</a>"
+                f"<a href='{reverse_lazy('cuac_core:batch-delete', kwargs={'pk': obj.pk})}' "
+                f"class='btn btn-danger'>"
+                f"<i class='bi bi-trash'></i>"
+                f"</a>")
+
+    def object_wrapper(self, obj):
+        row = {
+            'DT_RowId': obj.pk,
+            'title': obj.title,
+            'time': obj.time,
+            'hours_elapsed': obj.hours_elapsed,
+            'created': obj.created,
+            'expiration': obj.expiration,
+            'company_pk': obj.company.pk,
+            'company': obj.company.name,
+            'active': obj.active,
+            'invoiced': obj.invoiced,
+            'charged': obj.charged,
+        }
+        return row
