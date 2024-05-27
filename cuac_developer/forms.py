0ffedc07a82b1a15ac4130d53a96ad5152054685
julio <julioamayamlg@gmail.com>
2024-05-27 16:14:10 +0200

    Internationalization forms

--- cuac_developer/forms.py
+++ cuac_developer/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from cuac_core.wrapped_views import GenericCrispyForm
+from django.utils.translation import gettext_lazy as _
 from . import models
 
 
@@ -8,6 +9,17 @@ class BatchForm(GenericCrispyForm, forms.ModelForm):
     class Meta:
         model = models.Batch
         fields = '__all__'
+        labels ={
+            'name': _('Name'),
+            'time': _('Hours'),
+            'hours_elapsed': _('Hours elapsed'),
+            'created': _('Created'),
+            'expiration': _('Expires'),
+            'company': _('Client'),
+            'active': _('Active'),
+            'invoiced': _('Invoiced'),
+            'charged': _('Charged'),
+                }
 
 
 class TaskForm(GenericCrispyForm, forms.ModelForm):
@@ -15,3 +27,10 @@ class TaskForm(GenericCrispyForm, forms.ModelForm):
     class Meta:
         model = models.Task
         fields = '__all__'
+        labels ={
+            'title': _('Title'),
+            'description': _('Description'),
+            'time': _('Hours'),
+            'batch': _('Batch'),
+            'assignee': _('Assignee'),
+                }
